4440c82c6dd12af13c8f7b907f65d39f8b31fc2e
nazarboy <rakhmonovnazarboy@gmail.com>
2024-11-01 19:04:14 +0300

    before split

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class AboutModel(models.Model):
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.text[:20]}"
+
+    class Meta:
+        verbose_name = 'about'
+        verbose_name_plural = 'abouts'
